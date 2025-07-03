1c040e877c5c3ef02d5c1d42fa06ad1d9b2b093b
sajeel675 <maliksajeel675@gmail.com>
2025-07-04 00:06:51 +0500

    here is our js and html new practice code

---
+++ script1.js
@@ -0,0 +1,19 @@
+/*all sripting language are event base languages that create the event of clint side */
+var a,c;
+a=7;
+a=23+4;
+document.write(a);
+var a,b;
+a=8;
+b=12;
+b="here i defined var that is able to reasign and redeclear";
+document.write(a,b);
+/*up side defined variable in var can asign value again and declear angain*/
+/*under is the syntex of defining variable name*/
+document.write("start with word don't start with number exepmle: var sajeelAbbas");
+let nim,neo;
+nim=54;
+document.write(nim);
+neo=84;
+neo=2;
+document.write("we canlo")
